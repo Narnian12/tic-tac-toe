8e1f7db4d142747bc17136c46afc8a3c43358c7f
Peter Sun <pitaah@umich.edu>
2020-08-22 13:26:19 -0400

    - Optimize board initialization

--- ps_tic_tac_toe/src/index.js
+++ ps_tic_tac_toe/src/index.js
@@ -21,25 +21,20 @@ class Board extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+    let board = [];
+    for (var i = 0; i < 3; i++) {
+      let squares = [];
+      for (var j = 0; j < 3; j++) {
+        squares.push(this.renderSquare(j + i * 3));
+      }
+      // An interesting side-effect is that every element within an array needs to have a "key" value
+      board.push(
+        <div className="board-row" key={i}>
+          {squares}
         </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+      );
+    }
+    return <div>{board}</div>;
   }
 }
 
