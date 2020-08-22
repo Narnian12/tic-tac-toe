15f0fc6613b45a7a506dae21c3a8dd92a3af8b44
Peter Sun <pitaah@umich.edu>
2020-08-22 12:57:37 -0400

    - Implemented winner

--- ps_tic_tac_toe/src/index.js
+++ ps_tic_tac_toe/src/index.js
@@ -21,6 +21,8 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    // Optimize by not re-rendering if someone already won or if user clicked on the particular Square already
+    if (calculateWinner(squares) || squares[i]) return;
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
   }
@@ -35,8 +37,11 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-
+    // Determine winner
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) status = "Winner: " + winner;
+    else status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     return (
       <div>
         <div className="status">{status}</div>
@@ -79,3 +84,24 @@ class Game extends React.Component {
 // ==========================================
 
 ReactDOM.render(<Game />, document.getElementById("root"));
+
+// Helper function to determine winner
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
