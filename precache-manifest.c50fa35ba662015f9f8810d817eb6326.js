992ec0ecd795e68e96d81e23485a38fa65e8a43c
Peter Sun <pitaah@umich.edu>
2021-09-14 10:25:33 -0400

    Updates

---
+++ precache-manifest.c50fa35ba662015f9f8810d817eb6326.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b62b2fde4372fd1ef892bd1d2061b18",
+    "url": "./index.html"
+  },
+  {
+    "revision": "92e00f2b392dbcf62bd2",
+    "url": "./static/css/main.1f88b46a.chunk.css"
+  },
+  {
+    "revision": "cc7734bbfb790dabd46d",
+    "url": "./static/js/2.d18f4a25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d18f4a25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e00f2b392dbcf62bd2",
+    "url": "./static/js/main.e81b34a9.chunk.js"
+  },
+  {
+    "revision": "23bedb808eb06b70b3c7",
+    "url": "./static/js/runtime-main.1ddce69c.js"
+  }
+]);
